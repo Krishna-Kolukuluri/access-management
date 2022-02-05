409554713c0b992073172837663f2055cb37f427
Krishna-Kolukuluri <krish.kolukuluri@gmail.com>
2022-02-05 04:45:29 -0500

    Fixing H2 DB version

--- src/main/java/com/vmware/accessmanagement/model/User.java
+++ src/main/java/com/vmware/accessmanagement/model/User.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name ="Users")
+@Table(name ="User")
 @NoArgsConstructor
 @Getter
 @Setter
@@ -19,7 +19,6 @@ public class User {
     @Column(name ="id")
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     Long id;
-
 
     @Column(name = "user_name")
     String userName;
