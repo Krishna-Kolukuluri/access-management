9cc494c2ae9a26b42e1a917f06220070877406a9
Krishna-Kolukuluri <krish.kolukuluri@gmail.com>
2022-02-05 19:17:32 -0500

    Update UserControllerTests.java

--- src/test/java/com/vmware/accessmanagement/controller/UserControllerTests.java
+++ src/test/java/com/vmware/accessmanagement/controller/UserControllerTests.java
@@ -1,13 +1,16 @@
 package com.vmware.accessmanagement.controller;
 
 import com.vmware.accessmanagement.dto.UserDto;
+import com.vmware.accessmanagement.exception.ApiError;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import javax.transaction.Transactional;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -16,18 +19,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @Log4j2
 @Transactional
 public class UserControllerTests extends BaseTest {
-    @Test
-    public void testCreateUser() throws Exception {
-        UserDto userDto = new UserDto();
-        userDto.setFirstName("FName");
-        userDto.setLastName("LName");
-        userDto.setUserName("UName");
-        userDto.setIsAdmin(true);
-        String dob="31-12-1998";
+    private static UserDto userDto;
+
+    @BeforeAll
+    public static void setup() throws ParseException {
+        String dob="31-12-2010";
         Date date=new SimpleDateFormat("DD-MM-YYYY"). parse(dob);
+        userDto = new UserDto();
+        userDto.setFirstName("Krishna");
+        userDto.setLastName("Kolukuluri");
+        userDto.setUserName("Krishna.Kolukuluri");
+        userDto.setIsAdmin(true);
         userDto.setDob(date);
-        userDto.setAddress("XXXXXXX");
-        userDto.setPassword("XYZ");
+        userDto.setAddress("410 Windy Peak Loop, Cary, NC");
+        userDto.setPassword("Secret@123456");
+    }
+    @Test
+    public void testCreateUserWithValidUserNameAndPassword() throws Exception {
+        userDto.setUserName("Krishna.Kolukuluri");
+        userDto.setPassword("Secret@123456");
         String json = mapToJson(userDto);
         String uri = "/users/createUser";
         MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON).content(json)).andReturn();
@@ -35,5 +45,54 @@ public class UserControllerTests extends BaseTest {
         assertEquals(201, status);
         String content = mvcResult.getResponse().getContentAsString();
         assertEquals("Created User with User Name: '"+userDto.getUserName()+"'", content);
+    }
+
+    @Test
+    public void testCreateUserWithInValidUserName() throws Exception {
+        userDto.setUserName("Kris");
+        userDto.setPassword("Secret@123456");
+        String json = mapToJson(userDto);
+        String uri = "/users/createUser";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON).content(json)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(400, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        ApiError errors = mapFromJson(content, ApiError.class);
+        assertEquals( "Data Validation Constraints Exception", errors.getMessage());
+        assertEquals(1, errors.getErrors().size());
+        assertEquals("userName: Invalid User Name", errors.getErrors().get(0));
+    }
+
+    @Test
+    public void testCreateUserWithInValidPassword() throws Exception {
+        userDto.setPassword("Secretabcde");
+        userDto.setUserName("Krishna.Kolukuluri");
+        String json = mapToJson(userDto);
+        String uri = "/users/createUser";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON).content(json)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(400, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        ApiError errors = mapFromJson(content, ApiError.class);
+        assertEquals( "Data Validation Constraints Exception", errors.getMessage());
+        assertEquals(1, errors.getErrors().size());
+        assertEquals("password: Invalid Password", errors.getErrors().get(0));
+    }
+
+    @Test
+    public void testCreateUserWithInValidPasswordAndInvalidUsername() throws Exception {
+        userDto.setUserName("Kris");
+        userDto.setPassword("Secretabcde");
+        String json = mapToJson(userDto);
+        String uri = "/users/createUser";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON).content(json)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(400, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        ApiError errors = mapFromJson(content, ApiError.class);
+        assertEquals( "Data Validation Constraints Exception", errors.getMessage());
+        assertEquals(2, errors.getErrors().size());
+        assertEquals("password: Invalid Password", errors.getErrors().get(1));
+        assertEquals("userName: Invalid User Name", errors.getErrors().get(0));
     }
 }
