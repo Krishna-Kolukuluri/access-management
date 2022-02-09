5cc2686e1dac1f4a0fcbf100ba6507ee2e9b4e7c
KrishnaKolukuluri <krish.kolukuluri@live.com>
2022-02-08 23:07:12 -0500

    Creating Unique validator annotation

---
+++ src/main/java/com/vmware/accessmanagement/validator/UniqueValidator.java
@@ -0,0 +1,35 @@
+package com.vmware.accessmanagement;
+
+import com.vmware.accessmanagement.util.AppContext;
+import com.vmware.accessmanagement.validator.FieldValueExists;
+import com.vmware.accessmanagement.validator.Unique;
+import org.springframework.context.ApplicationContext;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+
+public class UniqueValidator implements ConstraintValidator<Unique, Object> {
+    private ApplicationContext applicationContext;
+
+    private FieldValueExists service;
+    private String fieldName;
+
+    @Override
+    public void initialize(Unique constraintAnnotation) {
+        Class<? extends FieldValueExists> clazz = constraintAnnotation.service();
+        this.fieldName = constraintAnnotation.fieldName();
+        String serviceQualifier = constraintAnnotation.serviceQualifier();
+        applicationContext = AppContext.getInstance().getContext();
+        if (!serviceQualifier.equals("")) {
+            this.service = this.applicationContext.getBean(serviceQualifier, clazz);
+        } else {
+            this.service = this.applicationContext.getBean(clazz);
+        }
+    }
+
+    @Override
+    public boolean isValid(Object o, ConstraintValidatorContext constraintValidatorContext) {
+        return !this.service.fieldValueExists(o, this.fieldName);
+    }
+}
