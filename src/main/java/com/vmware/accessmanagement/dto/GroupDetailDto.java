0a417ea09bbeeaed386af94876163fdf820ac0b4
KrishnaKolukuluri <krish.kolukuluri@gmail.com>
2022-02-23 22:01:21 -0500

    Adding validations to DTO's to validate user input and show it on api documentation

--- src/main/java/com/vmware/accessmanagement/dto/GroupDetailDto.java
+++ src/main/java/com/vmware/accessmanagement/dto/GroupDetailDto.java
@@ -5,15 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class GroupDetailDto extends GroupUpdateDto {
-    //private Long id;
+    //
+    @NotBlank(message = "groupName is mandatory")
     String groupName;
+    @NotBlank(message = "groupRole is mandatory")
     String groupRole;
+    @NotBlank(message = "groupPermission is mandatory")
     String groupPermission;
 
     public GroupDetailDto(GroupDetail groupDetail){
